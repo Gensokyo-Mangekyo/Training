ab32881c37068a7811f7e1a8930eb86ed49351d6
GensokyoPerson <samplay4ik@gmail.com>
2024-02-26 21:06:24 +0300

    FinishedAddParagraph

--- React/dev/src/App.js
+++ React/dev/src/App.js
@@ -1,32 +1,11 @@
 import React, { useState } from 'react'
 import "./styles/style.css"
 import Paragraph from './components/Paragraph'
+import AddParagraph from './components/AddParagraph';
 
 function App() {
-  const [edit,setEdit] = useState(false)
-  const [text,setText] = useState("")
-  const [textbox,setTextbox] = useState("Заголовок")
-  const [Paragraphs,setParagraphs] = useState([
-
-  ])
- 
-  if (edit === false)
   return (
-    <div className="App">
-      {Paragraphs.map((post,index) => <Paragraph key ={index} Options = {post} ></Paragraph>  )}
-      <img onClick={function() { setTextbox(text) ;setEdit(true);} } src='/images/Paragraph.png'></img>
-    </div>
-  );
-  else return(
-    <div className="App">
-     {Paragraphs.map((post,index) => <Paragraph key ={index} Options = {post} ></Paragraph>  )}
-
-    <textarea  onChange={function(event) {setText(event.target.value) } }></textarea>
-    <div className='buttons'>
-      <button  onClick={function() {setEdit(false); Paragraphs.push({Text: text}); } }>Сохранить</button>
-      <button onClick={function() {setEdit(false); setText(textbox);}}>Отменить</button>
-    </div>
-    </div>
+    <AddParagraph/>
   );
 }
 
