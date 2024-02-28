9f13c3cfa2875e82a217152cd97c6bd30f07433f
GensokyoPerson <samplay4ik@gmail.com>
2024-02-28 23:52:33 +0300

    EditableDiv

--- React/dev/src/App.js
+++ React/dev/src/App.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react'
 import "./styles/style.css"
-import Paragraph from './components/Paragraph'
-import AddParagraph from './components/AddParagraph';
+
 
 function App() {
   return (
-    <AddParagraph/>
+    <div>
+      <h1 id="Header">Текстовый редактор</h1>
+      <div id="buttons">
+      <button onClick={function() {
+        var selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+          var range = selection.getRangeAt(0);
+          var boldElement = document.createElement('strong');
+          boldElement.appendChild(range.extractContents());
+          range.insertNode(boldElement);
+        }
+      }}>Ж</button>
+      </div>
+          <div id="Editor" contenteditable="true" width="400" height="200" ></div>
+    </div>
   );
 }
 
