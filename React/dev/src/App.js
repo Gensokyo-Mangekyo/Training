065d4f31cd74ab9ee8b0050f28d27f285eff7d24
GensokyoPerson <samplay4ik@gmail.com>
2024-02-29 20:51:39 +0300

    StrongText

--- React/dev/src/App.js
+++ React/dev/src/App.js
@@ -9,11 +9,29 @@ function App() {
       <div id="buttons">
       <button onClick={function() {
         var selection = window.getSelection();
-        if (selection.rangeCount > 0) {
-          var range = selection.getRangeAt(0);
+        var range = selection.getRangeAt(0);
+        if (selection.rangeCount > 0) {        
+          const parentElement= selection.anchorNode.parentElement 
+          if (parentElement.tagName === "STRONG") {
+            var textNode = document.createTextNode(parentElement.innerHTML);
+            parentElement.replaceWith(textNode);
+          }
+          else {
+            var FoundTag = false
+            parentElement.childNodes.forEach(function(node) {
+              if (node.tagName === "STRONG") {
+                    FoundTag = true;
+                    var textNode = document.createTextNode(node.innerHTML);
+                    node.replaceWith(textNode);
+              }
+            })
+            if (!FoundTag) {
           var boldElement = document.createElement('strong');
           boldElement.appendChild(range.extractContents());
           range.insertNode(boldElement);
+            }
+          }
+        
         }
       }}>Ж</button>
       </div>
